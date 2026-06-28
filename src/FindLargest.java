fc83f0363a4972b90064c43c6b42fd2d0e66dff9
vandanatilve98-star <vandanatilve98@gmail.com>
2026-06-28 10:50:21 +0530

    added find largest element in Array Program

---
+++ src/FindLargest.java
@@ -0,0 +1,16 @@
+public class FindLargest {
+    public static void main(String[] args) {
+
+        int[] arr = {12, 45, 7, 89, 34};
+
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest Element = " + largest);
+    }
+}
