af283279d668e9467a8ffb34680d02c419f78c93
vandanatilve98-star <vandanatilve98@gmail.com>
2026-07-01 11:20:51 +0530

    collection 1st question

---
+++ src/CollectionDemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class CollectionDemo {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        System.out.println(fruits);
+        fruits.add("Mango");
+        System.out.println(fruits);
+    }
+}
