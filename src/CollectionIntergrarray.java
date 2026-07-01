1f89f71ce9b470d5bcf827ada8c56fdc5ec356a2
vandanatilve98-star <vandanatilve98@gmail.com>
2026-07-01 12:45:27 +0530

    2nd collection q Added ArrayList<Integer> creation program

---
+++ src/CollectionIntergrarray.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+class CollectionIntergrarray {
+    public static void main(String[]args){
+    ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+        System.out.println("Numbers: " + numbers);
+    }
+}
