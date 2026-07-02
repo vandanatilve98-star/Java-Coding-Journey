ac865d823b65607e4e6b65b78863d7368b5539eb
vandanatilve98-star <vandanatilve98@gmail.com>
2026-07-02 12:14:56 +0530

    3rdQ Added multiple String elements to ArrayList

---
+++ src/Collection_Array_3.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Collection_Array_3 {
+    public static void main(String[] args){
+       ArrayList<String>names = new ArrayList<String>();
+         names.add("kaira");
+         names.add("Anaaya");
+         names.add("Isha");
+        names.add("Kavya");
+       System.out.println("Names in the list: " + names);
+       System.out.println("Size of the list: " + names.size());
+    }
+}
