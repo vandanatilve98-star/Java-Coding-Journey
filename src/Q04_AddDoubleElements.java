1c9534f9a1b4f5a2ea0b76e699d71c326d0542b1
vandanatilve98-star <vandanatilve98@gmail.com>
2026-07-02 12:30:24 +0530

    Q06_AddDoubleElements.java

---
+++ src/Q04_AddDoubleElements.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Q04_AddDoubleElements {
+    public static void main(String[] args) {
+        ArrayList<Double> numbers = new ArrayList<>();
+
+        numbers.add(12.5);
+        numbers.add(45.3);
+        numbers.add(7.8);
+        numbers.add(89.1);
+        numbers.add(34.6);
+
+        System.out.println("Numbers in the list: " + numbers);
+        System.out.println("Size of the list: " + numbers.size());
+    }
+}
